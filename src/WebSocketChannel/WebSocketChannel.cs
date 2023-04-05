6443a2f8f3359f2d63cf8180f3e995bf552f8e71
Corrado Cavalli <corrado@outlook.com>
2023-04-05 19:36:28 +0200

    added additional check to optimize buffer expansion.

--- src/WebSocketChannel/WebSocketChannel.cs
+++ src/WebSocketChannel/WebSocketChannel.cs
@@ -249,14 +249,14 @@ static partial class WebSocketChannel
                         if (received.Count == 0)
                             break;
 
-                        if (!received.EndOfMessage)
+                        if (!received.EndOfMessage && count >= memoryOwner.Memory.Length)
                         {
                             var tempMem = memoryOwner.Memory.Slice(0, count);
                             memoryOwner = MemoryPool<byte>.Shared.Rent(count + 512);
                             tempMem.CopyTo(memoryOwner.Memory);
                         }
 
-                        received = await channel.webSocket.ReceiveAsync(memoryOwner.Memory.Slice(count), cancellation).ConfigureAwait(false);
+                        received = await channel.webSocket.ReceiveAsync(memoryOwner.Memory.Slice(count, memoryOwner.Memory.Length - count), cancellation).ConfigureAwait(false);
                         count += received.Count;
                     }
 
